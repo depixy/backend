099ae5cf0175d095396e28f65032cd779cedd44c
Joshua Avalon <git@avalon.sh>
2024-10-15 16:14:53 +0800

    feat: update

--- src/routes/__tests__/auth.ts
+++ src/routes/__tests__/auth.ts
@@ -5,36 +5,32 @@ import { loginName, password } from "./constant.js";
 import type { FastifyInstance } from "fastify";
 
 describe("Test routes", async () => {
-  describe("Test init", async () => {
+  describe("Test auth", async () => {
     let app: FastifyInstance;
     before(async () => {
       const cfg = await readConfig();
       app = await createApp(cfg);
     });
 
-    it("should POST /api/init", async () => {
+    it("should POST /api/auth/native/refresh-token", async () => {
       const payload = {
         loginName,
-        displayName: "test",
         password,
-        email: "test@example.com"
+        description: ""
       };
-      const res = await app.inject({ method: "POST", path: "/api/init", payload });
+      const res = await app.inject({ method: "POST", path: "/api/auth/native/refresh-token", payload });
       const json = await res.json();
       assert.equal(res.statusCode, 200);
       assert.equal(json.success, true);
-      assert.equal(json.loginName, loginName);
-      assert.isUndefined(json.password);
     });
 
-    it("should not POST /api/init after initialization", async () => {
+    it("should not POST /api/auth/native/refresh-token", async () => {
       const payload = {
         loginName,
-        displayName: "test",
-        password,
-        email: "test@example.com"
+        password: `${password}!`,
+        description: ""
       };
-      const res = await app.inject({ method: "POST", path: "/api/init", payload });
+      const res = await app.inject({ method: "POST", path: "/api/auth/native/refresh-token", payload });
       const json = await res.json();
       assert.equal(res.statusCode, 403);
       assert.equal(json.success, false);
